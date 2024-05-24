d84e629c7b5f0e71b1088cbd9ef5308735ad7539
Thu Nguyen <ntcamthu.chomoi@gmail.com>
2024-05-24 11:45:51 +0700

    Add viewUser, EditUser, DeleteUser

--- fullstack-backend/src/main/java/com/cruduser/fullstackbackend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/cruduser/fullstackbackend/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3001/")
+@CrossOrigin("http://localhost:3000/")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
@@ -26,6 +26,23 @@ public class UserController {
     @GetMapping("/user/{id}")
     User getUserById(@PathVariable Long id){
         return userRepository.findById(id).orElseThrow(()->new UserNotFoundException(id));
+    }
+    @PutMapping("user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user->{
+                    user.setUsername(newUser.getUsername());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+    @DeleteMapping("user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException((id));
+        }
+        userRepository.deleteById(id);
+        return "user witd id "+id+" deleted";
     }
 
 }
--- fullstack-frontend/crud-user/src/pages/Home.js
+++ fullstack-frontend/crud-user/src/pages/Home.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react'
 import axios from "axios"
+import { Link, useParams } from 'react-router-dom';
 
 
 export const Home = () => {
 
     const [users, setUsers] = useState([]);
+
+    const {id} = useParams();
     useEffect(() => {
         loadUsers();
     }, []);
@@ -14,6 +17,10 @@ export const Home = () => {
         setUsers(result.data);
     };
 
+    const deleteUser = async(id) => {
+        await axios.delete(`http://localhost:8080/user/${id}`)
+        loadUsers();
+    }
     return (
         <div className="container">
             <div className="py-4">
@@ -33,9 +40,13 @@ export const Home = () => {
                                     <td>{user.username}</td>
                                     <td>{user.email}</td>
                                     <td>
-                                        <button className="btn btn-primary mx-2">View</button>
-                                        <button className="btn btn-outline-primary mx-2">Edit</button>
-                                        <button className="btn btn-danger mx-2">Delete</button>
+                                        <Link className="btn btn-primary mx-2" to={`/detailuser/${user.id}`}>View</Link>
+                                        <Link className="btn btn-outline-primary mx-2"
+                                            to={`/edituser/${user.id}`}
+                                        >Edit</Link>
+                                        <button className="btn btn-danger mx-2"
+                                            onClick={()=>deleteUser(user.id)}
+                                        >Delete</button>
                                     </td>
                                 </tr>
                             ))
